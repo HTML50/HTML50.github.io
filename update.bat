ee74856b9224751ea0e920d7f5874b3d63af4b4c
2byte <iambenf@gmail.com>
2017-03-14 13:16:50 +0800

    blog update:

--- update.bat
+++ update.bat
@@ -1,10 +1,7 @@
 @echo off
 set p=%1
 
-if "%p%"=="" (
-echo 使用方法: %0  文章的摘要
-)
-else (
+if "%p%"=="" (echo= && echo 使用方法: %0  文章的摘要) else (
 cmd /c "make.bat %1"
 git add ./
 git commit -m "blog update: %1"
